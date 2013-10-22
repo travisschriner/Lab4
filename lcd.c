d274d1f1456531939a0e2f40a0c58f171310181a
Travis Schriner <C15Travis.Schriner@usafa.edu>
2013-10-21 22:22:51 -0600

    got my shell laid out and included the MSP430 library which had all the definitions for my pinouts. I am still having trouble with writing directly to a register and how to assign values to the variables I had to create...

--- lcd.c
+++ lcd.c
@@ -9,6 +9,26 @@
  *  that I would need.
  */
 
+#include "msp430.h"
+
+#define LCDDATA
+#define LCDSEND
+#define LCDCON
+
+void LCDinit();
+void LCDclear();
+void LCDDELAY1();
+void LCDDELAY2();
+void cursorToLineOne();
+void cursorToLineTwo();
+void writeChar(char asciiChar);
+void writeString(char * string);
+void scrollString(char * string1, char * string2);
+void setSSHi();
+void setSSLo();
+void SPISEND();
+void LCDWRT4();
+void LCDWRT8();
 
 //this is copied pretty much from Lab 3 example code
 void INITSPI(){
@@ -29,18 +49,86 @@ void INITSPI(){
 
 void LCDinit(){
 
+	setSSHi();
+
+	LCDCON = 0;
+
+	LCDDATA = 3;
+	LCDWRT4();
+	LCDDELAY2();
+
+	LCDDATA = 3;
+	LCDWRT4();
+	LCDDELAY1();
+
+	LCDDATA = 3;
+	LCDWRT4();
+	LCDDELAY1();
+
+	LCDDATA = 2;
+	LCDWRT4();
+	LCDDELAY1();
+
+	LCDSEND = 40;
+	LCDWRT8();
+	LCDDELAY2();
+
+	LCDSEND = 12;
+	LCDWRT8();
+	LCDDELAY2();
+
+	LCDSEND = 1;
+	LCDWRT8();
+	LCDDELAY1();
+
+	LCDSEND = 6;
+	LCDWRT8();
+	LCDDELAY2();
+
+	LCDSEND = 1;
+	LCDWRT8();
+	LCDDELAY2();
+
+	LCDSEND = 2;
+	LCDWRT8();
+	LCDDELAY2();
+
+	r5 = 0;
+	SPISEND();
+	LCDDELAY1();
+
+
 }
 
 void LCDclear(){
 
+	LCDCON = 0;
+	LCDSEND = 1;
+	LCDWRT8();
+	LCDDELAY1();
+	LCDCON = 64;
+	LCDDELAY2();
+
 }
 
+//40.5 us delay
 void LCDDELAY1(){
+
+}
+
+//1.65 ms delay
+void LCDDELAY2(){
 
 }
 
 void cursorToLineTwo(){
 
+	LCDCON = 0;
+	LCDSEND = 192;
+	LCDWRT8();
+	LCDDELAY1();
+	LCDCON = 64;
+	LCDDELAY2();
 }
 
 void cursorToLineOne(){
@@ -67,6 +155,13 @@ void setSSLo(){
 	P1OUT &= ~BIT0;
 }
 
+void SPISEND(){
+	setSSLo();
+	UCB0TXBUF = r5;
+	//need to complete all of wait1.
+	//I dont know how to pipe bit.b command
+
+}
 void LCDWRT4(){
 	LCDDATA = r5;
 	r5 += 15;
@@ -86,14 +181,20 @@ void LCDWRT4(){
 
 void LCDWRT8(){
 
+	r5 = LCDDATA;
+	r5 += 15;
+	r5 |= LCDCON;
+	r5 += 127;
+	SPISEND();
+	LCDDELAY1();
+	r5 |= 128;
+	SPISEND();
+	LCDDELAY1();
+	r5 += 127;
+	SPISEND();
+	LCDDELAY1();
 }
 
-void SPISEND(){
-	setSSLo();
-	UCB0TXBUF = r5;
-	//need to complete all of wait1.
-	//I dont know how to pipe bit.b command
 
-}
 
 
